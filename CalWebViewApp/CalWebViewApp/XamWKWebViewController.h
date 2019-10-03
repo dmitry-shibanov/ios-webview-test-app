7490bbccf5f211549ca2a1cb0d8ff47ce45860dc
Dmitry Shibanov <v-dmshib@microsoft.com>
2019-10-03 09:40:50 +0300

    remove import FLWebViewProvider

--- CalWebViewApp/CalWebViewApp/XamWKWebViewController.h
+++ CalWebViewApp/CalWebViewApp/XamWKWebViewController.h
@@ -1,6 +1,6 @@
 #import <UIKit/UIKit.h>
 #import <WebKit/WebKit.h>
-#import "FLWebViewProvider.h"
+// #import "FLWebViewProvider.h"
 
 @interface XamWKWebViewController : UIViewController
 <UIWebViewDelegate, WKNavigationDelegate, WKUIDelegate>
